4eebe92b81e41708efbbaa6ec2f3e55b13d1fac3
Thomas McConkey <tvmcconkey@gmail.com>
2023-06-28 13:10:42 -0500

    basic movement and intersection

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,57 @@
 #include <SFML/Window.hpp>
-#include <SFML/Window/VideoMode.hpp>
+#include <SFML/Graphics.hpp>
 
 #include <iostream>
+#include <vector>
 
 int main() {
-    std::cout << "yay" << std::endl;
-    sf::Window window = sf::Window(sf::VideoMode(sf::Vector2<unsigned int>(800, 600)), "SFML Window");
+    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode(sf::Vector2<unsigned int>(640, 640)), "SFML");
     window.setVerticalSyncEnabled(true);
+
+    sf::Texture atlas;
+    atlas.loadFromFile("assets/snake.png");
+
+    sf::Sprite apple(atlas, sf::IntRect({0, 0}, {16, 16}));
+    apple.scale({2.0f, 2.0f});
+
+    sf::Sprite head(atlas, sf::IntRect({16, 0}, {16, 16}));
+    head.scale({2.0f, 2.0f});
+
+    sf::Sprite body(atlas, sf::IntRect({32, 0}, {16, 16}));
+    body.scale({2.0f, 2.0f});
+    
+    int i = 0;
 
     while (window.isOpen()) {
         sf::Event event;
-
-
-
-
         while (window.pollEvent(event)) if (event.type == sf::Event::Closed) window.close();
-    }
 
-    std::cout << "yay" << std::endl;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            head.move({0.0f, -1.0f});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            head.move({-1.0f, 0.0f});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            head.move({0.0f, 1.0f});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            head.move({1.0f, 0.0f});
+        }
+
+        window.clear(sf::Color(200, 200, 255));
+
+        if (apple.getGlobalBounds().findIntersection(head.getGlobalBounds())) {
+            std::cout << "Intersect! " << i++ << std::endl;
+        }
+
+        body.setPosition({head.getPosition().x + 32.0f, head.getPosition().y});
+        window.draw(apple);
+        window.draw(head);
+        window.draw(body);
+        
+
+        window.display();
+
+    }
 }
